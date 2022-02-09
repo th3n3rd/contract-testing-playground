87c7bc68083bd01dd30e75122b942852dfacc13a
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-02-09 09:59:18 +0000

    Add http requests logging for all applications

---
+++ apps/cart/src/main/java/com/example/cart/Config.java
@@ -0,0 +1,21 @@
+package com.example.cart;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+class Config {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        var filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeHeaders(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludeClientInfo(true);
+        filter.setIncludePayload(true);
+        return filter;
+    }
+
+}
---
+++ apps/orders/src/main/java/com/example/orders/Config.java
@@ -0,0 +1,20 @@
+package com.example.orders;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+class Config {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        var filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeHeaders(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludeClientInfo(true);
+        filter.setIncludePayload(true);
+        return filter;
+    }
+
+}
