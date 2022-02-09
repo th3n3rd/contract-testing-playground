6ceb26f74987afa4d956dd1c616ba37bff1fbfb0
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-02-09 10:36:37 +0000

    Fix wrong use of random UUID so the contract does not generate always a new state to be provided at every build

--- apps/cart/src/test/java/com/example/cart/Fixtures.java
+++ apps/cart/src/test/java/com/example/cart/Fixtures.java
@@ -22,7 +22,7 @@ class Fixtures {
     }
 
     static class Products {
-        static UUID unknown = UUID.randomUUID();
+        static UUID unknown = UUID.fromString("fc19f260-fcce-4808-b8b2-470d06b49987");
         static UUID shirt = UUID.fromString("7f7b6b14-4034-429f-a286-e3946b135179");
         static UUID trousers = UUID.fromString("e6cc4361-8afa-496c-a602-900f66cf4c96");
     }
